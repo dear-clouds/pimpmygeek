dd09aa5ba2f534a7afd19bb2c6771902b9d12470
Mio <mio.bluewatermelon@gmail.com>
2017-02-17 14:11:31 +0100

    Db credentials

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql11.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql11159419'),
+            'username' => env('DB_USERNAME', 'sql11159419'),
+            'password' => env('DB_PASSWORD', 'eAuIXmJReg'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
